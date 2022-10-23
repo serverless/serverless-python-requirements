4ba3bbeb9296b4844feb476de695f33ee2a30056
Piotr Grzesik <pj.grzesik@gmail.com>
2022-10-23 22:50:04 +0200

    feat: Switch to official AWS docker images by default (#724)
    BREAKING CHANGE: Changes default `dockerImage` used for building dependencies

--- index.js
+++ index.js
@@ -95,7 +95,9 @@ class ServerlessPythonRequirements {
       );
     } else if (!options.dockerFile) {
       // If no dockerFile is provided, use default image
-      const defaultImage = `lambci/lambda:build-${this.serverless.service.provider.runtime}`;
+      const architecture =
+        this.serverless.service.provider.architecture || 'x86_64';
+      const defaultImage = `public.ecr.aws/sam/build-${this.serverless.service.provider.runtime}:latest-${architecture}`;
       options.dockerImage = options.dockerImage || defaultImage;
     }
     if (options.layer) {
